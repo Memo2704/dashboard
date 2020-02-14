f30163177b70ffaafd97f5e8aef1e4dcbc5f6648
Tony Arteaga <tony.arteaga@vaduz.com.ar>
2020-02-13 23:11:59 -0300

    Se crean las views para news app.

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -13,10 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from news.views import scrape
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^notes/', include('notepad.urls', namespace='notes'))
+    url(r'^notes/', include('notepad.urls', namespace='notes')),
+    url(r'^scrape/', scrape)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/news/views.py
+++ src/news/views.py
@@ -3,11 +3,17 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from datetime import timedelta, timezone, datetime
 import requests
 requests.packages.urllib3.disable_warnings()
 
+from .models import Headline, UserProfile
 
-def scrape():
+
+def scrape(request):
+    user_p = UserProfile.objects.filter(user=request.user).first()
+    user_p.last_scrape = datetime.now(timezone.utc)
+    user_p.save()
     session = requests.Session()
     session.headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
                                      "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -15,4 +21,19 @@ def scrape():
     url = "https://www.theonion.com/"
     content = session.get(url, verify=False).content
     soup = BeautifulSoup(content, "html.parser")
-    columns = soup.find_all('div', {'class': 'sc-1whp23a-1 iUsrYc'})
+    posts = soup.find_all('div', {'class': 'sc-1whp23a-1 iUsrYc'})
+
+    for i in posts:
+        link = i.find_all('a', {'class': 'sc-1pw4fyi-2 eoYCwA js_link sc-1out364-0 fwjlmD'})[0]['href']
+        title = i.find_all('h4', {'class': 'sc-1qoge05-0 ectNJs'})[0].text
+        image_source = i.find('img', {'class': 'dv4r5q-1 hEuYft'})['src']
+
+        if not image_source.startswith(("data:image", "javascript")):
+            media_root = '/home/tony/PycharmProjects/dashboard/media_root'
+            local_filename = image_source.split('/')[-1].split("?")[0]
+            r = session.get(image_source, stream=True, verify=False)
+            with open(local_filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=1024):
+                    f.write(chunk)
+            current_image_absolute_path = os.path.abspath(local_filename)
+            shutil.move(current_image_absolute_path, media_root)
