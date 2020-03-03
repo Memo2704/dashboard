f6bcd656757c65cc4448d09955a86a5c33966991
Tony Arteaga <tony.arteaga@vaduz.com.ar>
2020-03-02 23:37:46 -0300

    Finance continue.

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -19,14 +19,15 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from news.views import scrape, news_list
-from finance.views import company_article_list
+from finance.views import company_article_list, ChartData
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^notes/', include('notepad.urls', namespace='notes')),
     url(r'^scrape/', scrape, name='scrape'),
     url(r'^home/', news_list, name='home'),
-    url(r'^companies/', company_article_list, name='companies')
+    url(r'^companies/', company_article_list, name='companies'),
+    url(r'^api/chart/data/$', ChartData.as_view(), name='api-chart-data'),
 ]
 
 if settings.DEBUG:
